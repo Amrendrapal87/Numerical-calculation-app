03a45dcb0b006ed93e2c3b386a0de33a1b2533c4
Amrendra pal <amrendrapal7777@gmail.com>
2025-12-29 16:25:45 +0530

    Add files via upload

---
+++ style.js
@@ -0,0 +1,30 @@
+// sum  of numbers.....//
+
+function sumTwoNumbers(num1,num2){
+    return num1+num2;
+}
+console.log(sumTwoNumbers(1,3));
+
+// sub of numbser.....//
+
+function subTwoNumbers(num1,num2){
+    return num1-num2;
+}
+console.log(subTwoNumbers(1,3));
+
+// Div of number ....//
+
+function divTwoNumbers(num1,num2){
+    return num1/num2;
+}
+console.log(divTwoNumbers(1,3));
+
+// Mult of number ....// 
+
+
+function multTwoNumbers(num1,num2){
+    return num1*num2;
+}
+console.log(multTwoNumbers(1,3));
+
+
